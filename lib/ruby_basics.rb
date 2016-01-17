04b6a4baeb2758d7c5c8da5ff75ba5ec00d8f11d
CodeByLine <yleventhal@ymail.com>
2016-01-17 19:54:49 +0000

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,40 @@
+require 'pry'
+
 def division(num1, num2)
- 
+
+  return num1/num2
+
 end
 
 def assign_variable(value)
-  
+  return value
 end
 
-def argue
-  
+
+def argue(phrase)
+  return phrase
 end
 
-def greeting
+def greeting(greeting, name)
+  return greeting
 
 end
 
 def return_a_value
-
+  return "Nice"
 end
 
 def last_evaluated_value
+  last_value = "Bob"
+  last_value = "Jon"
+  last_value = "expert"
+  return last_value
 
 end
 
-def pizza_party
+def pizza_party(food = "cheese")
 
+  return food
 end
+
+pizza_party("pepperoni")
